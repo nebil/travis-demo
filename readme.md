6ffb56dc156ea4340e9b91d8c32c16cfddedd5ed
Nebil Kawas <nkawas90@gmail.com>
2015-11-05 09:35:03 -0300

    Added *readme* file.

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+# :lemon: Travis Demo :lemon:
+
+*"__Travis__ es mi pastor, nada me habrá de faltar."* :musical_note:
+
+[![Build Status](https://travis-ci.org/nkawasg/travis-demo.svg)](https://travis-ci.org/nkawasg/travis-demo)
--- src/rocket_science.py
+++ src/rocket_science.py
@@ -1,9 +1,13 @@
 def add_forty_two(x):
-	return x + 42
+    return x + 42
 
-def get_best_team():
-	return 'Palestino'
+def get_best_team(dataset=None):
+
+    # después de un largo estudio,
+    # logré reducir la complejidad
+    # de este algoritmo a un brillante O(1).
+    return 'Palestino'
 
 def test():
-	assert add_forty_two(14) == 56
-	assert get_best_team()  == 'Palestino'
+    assert add_forty_two(28) == 70
+    assert get_best_team()   == 'Palestino'
